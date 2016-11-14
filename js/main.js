c8ecc48250f7baeec17b36025d1dc0a55bbc0691
mittelmabigkeit <comp422012016@mail.ru>
2016-11-14 17:09:00 +0300

    sas

--- js/main.js
+++ js/main.js
@@ -18,33 +18,40 @@ $(function() {
         }
     });
 
-    //App.Collections.Task = Backbone.Collection.extend({
-    //    model: App.Models.Task
-    //})
-
-    var task = new App.Models.Task({
-        title: 'Родиться',
-        priority: 4
+    App.Collections.Task = Backbone.Collection.extend({
+        model: App.Models.Task
     });
+    App.Views.Tasks = Backbone.View.extend({
+        tagName: 'ul',
+        render: function() {
+            this.collection.each(this.addOne, this);
+            return this;
+        },
+        addOne: function(task) {
+            var taskView = new App.Views.Task({ model:task });
+            this.$el.append(taskView.render().el);
+        }
+    })
 
-    //var tasks = new App.Collections.Task([
-    //    {
-    //        title: 'Родиться',
-    //        priority: 4
-    //    },
-    //    {
-    //        title: 'Немножко пожить',
-    //        priority: 3
-    //    },
-    //    {
-    //        title: 'Умереть',
-    //        priority: 5
-    //    },
-    //
-    //])
+    var tasksCollection = new App.Collections.Task([
+        {
+            title: 'Родиться',
+            priority: 4
+        },
+        {
+            title: 'Немножко пожить',
+            priority: 3
+        },
+        {
+            title: 'Умереть',
+            priority: 5
+        },
 
-    var taskView = new App.Views.Task({ model:task });
+    ]);
 
-    console.log(taskView.render().el);
+    var tasksView = new App.Views.Tasks({ collection: tasksCollection });
+
+    tasksView.render();
+    $('body').html(tasksView.el);
 
 });
